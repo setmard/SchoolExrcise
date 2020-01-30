d3608dac946bccaca2c4c1839763a59c26605b02
setmard <sergioe.martinez@softtek.com>
2020-01-30 14:00:27 -0600

    se creo conexion a DB, permite leer los datos de la tabla school

---
+++ SchoolExerciseMVC/Controllers/SchoolController.cs
@@ -0,0 +1,33 @@
+﻿using SchoolExerciseMVC.Models;
+using SchoolExerciseMVC.Models.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolExerciseMVC.Controllers
+{
+    public class SchoolController : Controller
+    {
+        // GET: School
+        public ActionResult Index()
+        {
+            List<SchoolList> lst;
+            using (SchoolEntities db = new SchoolEntities())
+            {
+                lst = (from d in db.School
+                       select new SchoolList
+                       {
+                           Id = d.id,
+                           Name = d.name,
+                           Year = d.year
+
+                       }).ToList();
+            }
+
+            return View(lst);
+        }
+    }
+       
+}
---
+++ SchoolExerciseMVC/Models/ViewModel/SchoolList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SchoolExerciseMVC.Models.ViewModel
+{
+    public class SchoolList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Year { get; set; }
+    }
+}
